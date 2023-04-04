93493e0b1ce3944b321f1c1f2d85e2d15ca5c044
David Radjai <dradjai@yahoo.com>
2023-04-04 12:26:37 -0400

    Post-API-Firestore-db

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+import { addNewAnimal } from './src/animals.js';
+
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // tells express that our POSTS will be JSON
+
+
+//app.get('/animals', getAllAnimals)
+app.post('/animals', addNewAnimal)
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
+
---
+++ src/animals.js
@@ -0,0 +1,8 @@
+import { db } from "./dbConnect.js";
+
+export function addNewAnimal(req, res) {
+  const newAnimal = req.body
+  db.collection('animals').add(newAnimal)
+    .then(doc => res.status(201).send("New Animal Added: " + doc.id))
+    .catch(err => res.status(500).send(err)) 
+}
